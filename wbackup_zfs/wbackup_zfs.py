1908394b613d5d6d760736e91dab63dfa9972dbc
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2024-09-09 12:55:02 +0200

    add python inline script metadata per https://packaging.python.org/en/latest/specifications/inline-script-metadata/#inline-script-metadata

--- wbackup_zfs/wbackup_zfs.py
+++ wbackup_zfs/wbackup_zfs.py
@@ -14,6 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# /// script
+# requires-python = ">=3.7"
+# dependencies = []
+# ///
+
 import argparse
 import collections
 import operator
