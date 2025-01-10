f5ab0ecf2dfced6609870b0c296828f6c6e9ab08
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2025-01-10 11:20:36 +0100

    add convenience shell script to run update_readme.py
    Signed-off-by: Wolfgang Hoschek <wolfgang.hoschek@mac.com>

---
+++ update_readme.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# Run this script to update README.md from the help info contained in bzfs.py.
+set -e
+cd $(realpath $(dirname "$0"))
+python3 -m bzfs_docs.update_readme bzfs/bzfs.py README.md
