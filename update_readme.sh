9136968d2c74174a72369efeffc2bc2bc29dde93
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2025-04-03 18:47:12 -0400

    fix PATH to generate docs
    Signed-off-by: Wolfgang Hoschek <wolfgang.hoschek@mac.com>

--- update_readme.sh
+++ update_readme.sh
@@ -2,5 +2,6 @@
 # Run this script to update README.md from the help info contained in bzfs.py.
 set -e
 cd $(realpath $(dirname "$0"))
+export PATH=bzfs:$PATH
 python3 -m bzfs_docs.update_readme bzfs/bzfs.py README.md
 python3 -m bzfs_docs.update_readme bzfs/bzfs_jobrunner.py README_bzfs_jobrunner.md
