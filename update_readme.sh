2a215b969ec574c82b0aba21e4a6e166522d7150
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2025-05-25 03:24:05 -0400

    make the bash completion script print a list of valid arguments for the current CLI option on <TAB>

--- update_readme.sh
+++ update_readme.sh
@@ -5,4 +5,4 @@ cd $(realpath $(dirname "$0"))
 export PATH=bzfs:$PATH
 python3 -m bzfs_docs.update_readme bzfs/bzfs.py README.md
 python3 -m bzfs_docs.update_readme bzfs/bzfs_jobrunner.py README_bzfs_jobrunner.md
-./bash_completion.d/shell-completion-generator.sh > ./bash_completion.d/bzfs-shell-completion
+./bash_completion.d/shell-completion-generator.py > ./bash_completion.d/bzfs-shell-completion
