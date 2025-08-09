25bc7c9822bd35c4f09a306988f0ef4e49fe2fe2
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2025-08-09 02:24:38 -0400

    test: run nightly hpnssh tests on zfs-2.2.8 instead of zfs-2.3.1 (cont'd)
    Signed-off-by: Wolfgang Hoschek <wolfgang.hoschek@mac.com>

--- bzfs_tests/test_integrations.py
+++ bzfs_tests/test_integrations.py
@@ -5606,7 +5606,7 @@ class FullRemoteTestCase(MinimalRemoteTestCase):
         self.inject_pipe_error("inject_src_pipe_fail", expected_error=[1, DIE_STATUS])
 
     def test_inject_src_pipe_garble(self) -> None:
-        if is_pv_at_least_1_9_0() and is_zfs_at_least_2_3_0():
+        if is_pv_at_least_1_9_0():
             self.skipTest("workaround for zfs send-receive pipeline hang")
         self.inject_pipe_error("inject_src_pipe_garble")
 
