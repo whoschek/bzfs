75f3c2d78806a1ffeb03bdc90f04e577b9eee6fb
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2026-03-22 19:24:32 -0400

    test: docker2
    Signed-off-by: Wolfgang Hoschek <wolfgang.hoschek@mac.com>

--- bzfs_testbed/docker_example.sh
+++ bzfs_testbed/docker_example.sh
@@ -14,9 +14,10 @@ sudo chmod 600 /tmp/mybzfs-root-ssh/authorized_keys
 export BZFS_GIT_TAG="v1.19.0"  # adjust as desired
 export BZFS_DOCKER_OS="ubuntu-24.04"
 export BZFS_DOCKER_IMAGE="docker.io/whoschek/bzfs:${BZFS_GIT_TAG}-${BZFS_DOCKER_OS}"
+DOCKER_CLI="$(command -v nerdctl || command -v docker)"  # Lima includes nerdctl which is compatible with docker CLI in this context
 
-sudo nerdctl rm -f mybzfs > /dev/null 2>&1 || true
-sudo nerdctl run -d --name mybzfs \
+sudo "$DOCKER_CLI" rm -f mybzfs > /dev/null 2>&1 || true
+sudo "$DOCKER_CLI" run -d --name mybzfs \
     --publish=2222:2222 \
     --mount type=bind,src=/etc/ssh,dst=/etc/ssh,readonly \
     --mount type=bind,src=/etc/hpnssh,dst=/etc/hpnssh,readonly \
@@ -29,6 +30,6 @@ sleep 1
 
 ssh -p 2222 root@127.0.0.1 'zfs list'
 
-sudo nerdctl rm -f mybzfs > /dev/null 2>&1 || true
+sudo "$DOCKER_CLI" rm -f mybzfs > /dev/null 2>&1 || true
 
 printf 'Success!\n'
