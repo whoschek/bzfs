6b5a20b4b98426ef317ccd0151cc6990254ccc12
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2026-03-22 19:54:00 -0400

    test: docker2
    Signed-off-by: Wolfgang Hoschek <wolfgang.hoschek@mac.com>

--- bzfs_testbed/docker_example.sh
+++ bzfs_testbed/docker_example.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
-
 set -e
-sudo rm -fr /etc/hpnssh
+
+# configure ~/.ssh/ for the docker container (will be bind-mounted)
+sudo rm -rf /bzfs.ssh
+sudo mkdir -p /bzfs.ssh
+cat ~/.ssh/id_rsa.pub | sudo tee /bzfs.ssh/authorized_keys > /dev/null
+sudo chmod u=rw,go= /bzfs.ssh/authorized_keys
+
+# configure /etc/hpnssh/ for the docker container (will be bind-mounted)
+sudo rm -rf /etc/hpnssh
 sudo rsync -a /etc/ssh/ /etc/hpnssh
 sudo sed -i -E '/^[[:space:]]*#?[[:space:]]*Port[[:space:]]+[0-9]+/d' /etc/hpnssh/sshd_config  # remove existing ports
 printf '%s\n' "Port 2222" | sudo tee -a /etc/hpnssh/sshd_config > /dev/null  # add port 2222
 sudo sed -i 's#/etc/ssh#/etc/hpnssh#g' /etc/hpnssh/sshd_config  # change all occurrences of /etc/ssh to /etc/hpnssh
 sudo sed -i -E 's|^([[:space:]]*)Include([[:space:]]+)|\1# Include\2|' /etc/hpnssh/sshd_config  # comment out Include directives
 
-sudo install -d -m 700 /tmp/mybzfs-root-ssh
-cat ~/.ssh/id_rsa.pub | sudo tee /tmp/mybzfs-root-ssh/authorized_keys > /dev/null  # authorize the matching key for root
-sudo chmod 600 /tmp/mybzfs-root-ssh/authorized_keys
-
 DOCKER_CLI="$(command -v nerdctl || command -v docker)"  # Lima includes nerdctl which is compatible with docker CLI in this context
 sudo "$DOCKER_CLI" compose -f docker-compose.yml up -d
 sleep 1
-ssh -p 2222 root@127.0.0.1 'zfs list'
+ssh -p 2222 root@127.0.0.1 'zfs list'  # verify
 sudo "$DOCKER_CLI" compose -f docker-compose.yml down
 printf 'Success!\n'
