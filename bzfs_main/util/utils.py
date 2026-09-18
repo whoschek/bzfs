f1491d8f92e924f2f55305b67ba8298740c6eec4
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2026-09-18 12:47:22 -0400

    style: improve readability
    Signed-off-by: Wolfgang Hoschek <wolfgang.hoschek@mac.com>

--- bzfs_main/util/utils.py
+++ bzfs_main/util/utils.py
@@ -317,11 +317,11 @@ def percent(number: int, total: int, *, print_total: bool = False) -> str:
 def open_nofollow(
     path: str,
     mode: str = "r",
+    *,
     buffering: int = -1,
     encoding: str | None = None,
     errors: str | None = None,
     newline: str | None = None,
-    *,
     perm: int = FILE_PERMISSIONS,
     check_owner: bool = True,
     check_perm: bool = True,
@@ -478,7 +478,7 @@ def is_descendant(dataset: str, of_root_dataset: str) -> bool:
     return dataset == of_root_dataset or dataset.startswith(of_root_dataset + "/")
 
 
-def has_prefix(sorted_strings: list[str], prefix: str, is_test_mode: bool = False) -> bool:
+def has_prefix(sorted_strings: list[str], prefix: str, *, is_test_mode: bool = False) -> bool:
     """Returns whether any string in lexicographically sorted_strings starts with the given prefix."""
     assert (not is_test_mode) or sorted_strings == sorted(sorted_strings), "List is not sorted"
     i = bisect.bisect_left(sorted_strings, prefix)
